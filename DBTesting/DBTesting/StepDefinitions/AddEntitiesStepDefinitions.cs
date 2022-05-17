a8040e6c099da2371187802050d3cf7b94d7c414
Asparuh Petrov <asparuh.petrov@gmail.com>
2022-05-17 15:07:14 +0300

    added logic for mutating object for compare

--- DBTesting/DBTesting/StepDefinitions/AddEntitiesStepDefinitions.cs
+++ DBTesting/DBTesting/StepDefinitions/AddEntitiesStepDefinitions.cs
@@ -1,5 +1,7 @@
 using DBTesting.DataContext;
 using DBTesting.DBContext;
+using DBTesting.Models;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 
@@ -31,7 +33,24 @@ namespace DBTesting.StepDefinitions
         [Then(@"I should be able to see that user in DB")]
         public void ThenIShouldBeAbleToSeeThatUserInDB()
         {
-            Assert.That(_repo.Repository.Contains(p => p.Email == TestData.DefaultUser.Email));
+            var userFromDBLastID = _scenarioContext.Get<int>("lastUserID");
+            var userFromDB = _repo.Repository.Get(userFromDBLastID);
+
+            var defaultUserData = JsonConvert.DeserializeObject<UserEntity>(JsonConvert.SerializeObject(TestData.DefaultUser));
+
+            _repo.Repository.Reload(userFromDB);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(userFromDB.Title, Is.EqualTo(defaultUserData.Title), "Title does not match");
+                Assert.That(userFromDB.FirstName, Is.EqualTo(defaultUserData.FirstName), "FirstName does not match");
+                Assert.That(userFromDB.SirName, Is.EqualTo(defaultUserData.SirName), "SirName does not match");
+                Assert.That(userFromDB.Country, Is.EqualTo(defaultUserData.Country), "Country does not match");
+                Assert.That(userFromDB.City, Is.EqualTo(defaultUserData.City), "City does not match");
+                Assert.That(userFromDB.Email, Is.EqualTo(defaultUserData.Email), "Email does not match");
+                Assert.That(userFromDB.Password, Is.EqualTo(defaultUserData.Password), "Password does not match");
+                Assert.That(userFromDB.IsAdmin, Is.EqualTo(defaultUserData.IsAdmin), "User admin rights are not correct");
+            });
         }
     }
 }
