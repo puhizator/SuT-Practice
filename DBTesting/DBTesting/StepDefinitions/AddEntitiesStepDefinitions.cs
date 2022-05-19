2c0a816c037781ae8e8281ac6dcae0347acdcf84
Asparuh Petrov <asparuh.petrov@gmail.com>
2022-05-19 17:13:23 +0300

    Some changes in Add user step defs

--- DBTesting/DBTesting/StepDefinitions/AddEntitiesStepDefinitions.cs
+++ DBTesting/DBTesting/StepDefinitions/AddEntitiesStepDefinitions.cs
@@ -27,9 +27,11 @@ namespace DBTesting.StepDefinitions
         [When(@"I add single Entity")]
         public void WhenIAddSingleEntity()
         {
-            _repo.Repository.Create(TestData.DefaultUser);
+            var userToAdd = TestData.GenerateNewUser();
+            _repo.Repository.Create(userToAdd);
 
-            _scenarioContext.Add("lastUserID", TestData.DefaultUser.Id);
+            _scenarioContext.Add("lastUserID", userToAdd.Id);
+            _scenarioContext.Add("lastUser", userToAdd);
         }
 
         [When(@"I add multiple entities")]
@@ -60,22 +62,23 @@ namespace DBTesting.StepDefinitions
         public void ThenIShouldBeAbleToSeeThatUserInDB()
         {
             var userFromDBLastID = _scenarioContext.Get<int>("lastUserID");
+            var userSavedInScenario = _scenarioContext.Get<UserEntity>("lastUser");
             var userFromDB = _repo.Repository.Get(userFromDBLastID);
 
-            var defaultUserData = JsonConvert.DeserializeObject<UserEntity>(JsonConvert.SerializeObject(TestData.DefaultUser));
+            var initialUser = JsonConvert.DeserializeObject<UserEntity>(JsonConvert.SerializeObject(userSavedInScenario));
 
             _repo.Repository.Reload(userFromDB);
 
             Assert.Multiple(() =>
             {
-                Assert.That(userFromDB.Title, Is.EqualTo(defaultUserData.Title), "Title does not match");
-                Assert.That(userFromDB.FirstName, Is.EqualTo(defaultUserData.FirstName), "FirstName does not match");
-                Assert.That(userFromDB.SirName, Is.EqualTo(defaultUserData.SirName), "SirName does not match");
-                Assert.That(userFromDB.Country, Is.EqualTo(defaultUserData.Country), "Country does not match");
-                Assert.That(userFromDB.City, Is.EqualTo(defaultUserData.City), "City does not match");
-                Assert.That(userFromDB.Email, Is.EqualTo(defaultUserData.Email), "Email does not match");
-                Assert.That(userFromDB.Password, Is.EqualTo(defaultUserData.Password), "Password does not match");
-                Assert.That(userFromDB.IsAdmin, Is.EqualTo(defaultUserData.IsAdmin), "User admin rights are not correct");
+                Assert.That(userFromDB.Title, Is.EqualTo(initialUser.Title), "Title does not match");
+                Assert.That(userFromDB.FirstName, Is.EqualTo(initialUser.FirstName), "FirstName does not match");
+                Assert.That(userFromDB.SirName, Is.EqualTo(initialUser.SirName), "SirName does not match");
+                Assert.That(userFromDB.Country, Is.EqualTo(initialUser.Country), "Country does not match");
+                Assert.That(userFromDB.City, Is.EqualTo(initialUser.City), "City does not match");
+                Assert.That(userFromDB.Email, Is.EqualTo(initialUser.Email), "Email does not match");
+                Assert.That(userFromDB.Password, Is.EqualTo(initialUser.Password), "Password does not match");
+                Assert.That(userFromDB.IsAdmin, Is.EqualTo(initialUser.IsAdmin), "User admin rights are not correct");
             });
         }
 
