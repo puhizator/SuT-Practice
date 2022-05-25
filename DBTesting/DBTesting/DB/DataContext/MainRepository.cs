97d42b6e4f163a9f30098d5e0d213d660718dc9e
Asparuh Petrov <asparuh.petrov@gmail.com>
2022-05-25 16:54:23 +0300

    Added Rest folder. implemented Get rest requests with asserts

--- DBTesting/DBTesting/DB/DataContext/MainRepository.cs
+++ DBTesting/DBTesting/DB/DataContext/MainRepository.cs
@@ -7,12 +7,12 @@ namespace DBTesting.DB.DataContext
 {
     public class MainRepository
     {
-        public BaseClient<SuTContext, UserEntity> Repository { get; }
+        public BaseDBClient<SuTContext, UserEntity> Repository { get; }
 
         public MainRepository()
         {
             var connection = ConfigurationProvider.GetValue[Labels.DBConnectionString];
-            Repository = new BaseClient<SuTContext, UserEntity>(connection);
+            Repository = new BaseDBClient<SuTContext, UserEntity>(connection);
         }
     }
 }
---
+++ DBTesting/DBTesting/REST/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DBTesting.REST.Models
+{
+    internal class User
+    {
+        [JsonPropertyName("city")]
+        public object City { get; set; }
+
+        [JsonPropertyName("country")]
+        public object Country { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("is_admin")]
+        public int IsAdmin { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("sir_name")]
+        public string SirName { get; set; }
+
+        [JsonPropertyName("title")]
+        public object Title { get; set; }
+    }
+}
