3429c36682f22ba82fde237c725ae3658a294e2d
Asparuh Petrov <asparuh.petrov@gmail.com>
2022-05-31 15:41:48 +0300

    dsad

---
+++ DBTesting/DBTesting/WEB/WebContext/WebDriverProvider.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace DBTesting.WEB.WebContext
+{
+    internal class WebDriverProvider
+    {
+        private static IWebDriver _driver;
+        internal static void InitChromeDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            options.AddArgument("incognito");
+            _driver = new ChromeDriver(options);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        }
+
+        internal static IWebDriver GetChromeDriver()
+        {
+            return _driver;
+        }
+    }
+}
---
+++ DBTesting/DBTesting/WEB/WebHooks/Hooks.cs
@@ -0,0 +1,20 @@
+﻿using DBTesting.WEB.WebContext;
+using TechTalk.SpecFlow;
+
+namespace DBTesting.WEB.WebHooks
+{
+    internal sealed class Hooks
+    {
+        [BeforeScenario("web")]
+        private void FirstBeforeScenario()
+        {
+            WebDriverProvider.InitChromeDriver();
+        }
+
+        [AfterScenario("web")]
+        private void AfterScenario()
+        {
+            WebDriverProvider.GetChromeDriver().Dispose();
+        }
+    }
+}
