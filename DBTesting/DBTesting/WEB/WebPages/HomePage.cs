fa905104cb7b479c97971604fc80d2fadbe8a069
Asparuh Petrov <asparuh.petrov@gmail.com>
2022-05-31 16:46:59 +0300

    added BaseUserAction class added WebHooks added WebContext added WebPages

---
+++ DBTesting/DBTesting/WEB/WebPages/HomePage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBTesting.WEB.WebPages
+{
+    internal class HomePage
+    {
+    }
+}
